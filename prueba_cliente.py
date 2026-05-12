9b8f9cd639d6f96d3aece3a1ebfbc476b65c5043
Valentina Rodriguez <vrodriguezm48@unisalle.edu.co>
2026-05-11 22:01:47 -0500

    Agrego clase ClienteRemitente y archivo de prueba

---
+++ prueba_cliente.py
@@ -0,0 +1,14 @@
+from cliente_remitente import ClienteRemitente
+cliente1 = ClienteRemitente(
+    "Kevin",
+    "Rodriguez",
+    "Colombia",
+    "kevin@email.com",
+    "123456789",
+    "3232376126",
+    "Bogotá",
+    "Estudiante",
+    "Salario"
+)
+
+print(cliente1.mostrar_info())
