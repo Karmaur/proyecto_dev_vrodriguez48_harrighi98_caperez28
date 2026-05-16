0916b5fb53585a8eeb3a2d38d93a01bfbf3b4c26
Valentina Rodriguez <vrodriguezm48@unisalle.edu.co>
2026-05-16 12:26:04 -0500

    Agrego el mostrar a la clase

--- cliente_destino.py
+++ cliente_destino.py
@@ -5,3 +5,5 @@ class ClienteDestino:
         self.pais = pais
         self.telefono = telefono
         self.correo = correo_electronico
+    def mostrar_info(self):
+           return f"{self.nombre} {self.apellido} - {self.correo} - Tel: {self.telefono}"
---
+++ prueba_clientedestino.py
@@ -0,0 +1,10 @@
+from cliente_destino import ClienteDestino 
+destinatario1= ClienteDestino(
+    "Jhon",
+    "Smith",
+    "Estados Unidos",
+    "800 555 0125",
+    "jolin.doe@email.com",
+)
+
+print(destinatario1.mostrar_info())
