8674c2fabb21e5d0a4e626b5ae7406c12e332f55
Valentina Rodriguez <vrodriguezm48@unisalle.edu.co>
2026-05-16 12:11:56 -0500

    Agrego clase cliente destino

---
+++ cliente_destino.py
@@ -0,0 +1,7 @@
+class ClienteDestino:
+    def __init__(self, nombre, apellido, pais, telefono, correo_electronico):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.pais = pais
+        self.telefono = telefono
+        self.correo = correo_electronico
