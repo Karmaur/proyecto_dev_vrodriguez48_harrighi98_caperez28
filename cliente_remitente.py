38eaab32d5e38c5d4f27883ed55cf49f288ff442
hiar-10 <harrighi98@unisalle.edu.co>
2026-05-24 10:57:41 -0500

    fix: correct ISO4217.xml file path

---
+++ cliente_remitente.py
@@ -0,0 +1,14 @@
+class ClienteRemitente:
+    def __init__(self, nombre, apellido, pais, correo, documento, telefono, direccion, ocupacion, origen_fondos):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.pais = pais
+        self.correo = correo
+        self.documento = documento
+        self.telefono = telefono
+        self.direccion = direccion
+        self.ocupacion = ocupacion
+        self.origen_fondos = origen_fondos
+
+    def mostrar_info(self):
+           return f"{self.nombre} {self.apellido} - {self.correo} - Tel: {self.telefono}"
